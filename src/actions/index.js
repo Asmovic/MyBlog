a198df78ac307c72e39dbe653e0673d564371acd
Asmovic <Asogba007@gmail.com>
2019-05-22 16:22:38 +0100

    second commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
 export const DELETE_POST = 'delete_post';
 import axios from 'axios';
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=asmovic123';
 
 export function fetchPosts() {
